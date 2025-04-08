ba2654e67e76a3773f60c94843d516a9ecfee8b8
Matt Payne <pattmayne@gmail.com>
2025-04-08 16:26:22 -0400

    printing symbols proper

--- main.nim
+++ main.nim
@@ -1,6 +1,39 @@
 import csfml
 import random
 
+type Point = object
+  x: int
+  y: int
+
+proc newPoint(newX, newY: int): Point = 
+  result.x = newX
+  result.y = newY
+
+
+proc getSymbolPoints(number_of_points, screen_width:int): seq[Point] =
+  let is_even : bool = (number_of_points mod 2) == 0
+  let number_of_pairs: int = number_of_points div 2
+  let screen_center : int = screen_width div 2
+  let spacing = int(screen_width / (number_of_points + 2))
+  var pairs_counter:int = 0
+
+  while pairs_counter < number_of_pairs:
+    let distance_from_center: int = spacing * (number_of_pairs - pairs_counter)
+    let point_left_x : int = screen_center - distance_from_center
+    let point_right_x : int = screen_center + distance_from_center
+    let point_y : int = spacing * (number_of_pairs - pairs_counter)
+
+    let point_left : Point = newPoint(point_left_x, point_y)
+    let point_right : Point = newPoint(point_right_x, point_y)
+
+    result.add(point_left)
+    result.add(point_right)
+
+    pairs_counter += 1
+
+  if not is_even:
+    let centre_point = newPoint(screen_center, 100)
+    result.add(centre_point)
 
 var window = new_RenderWindow(video_mode(800, 600), "Timely Tarot")
 window.vertical_sync_enabled = true
@@ -19,18 +52,25 @@ bird.scale = vec2(0.7, 0.7)
 bird.position = vec2(window.size.x/3, window.size.y/2)
 
 var bg = new_Sprite(bg_texture)
+bg.scale = vec2(1.22, 1.22)
 bg.position = vec2(0, 0)
 
 var pentacle = new_Sprite(pentacle_texture)
 let pent_scale: float = 0.25
 pentacle.scale = vec2(pent_scale, pent_scale)
-pentacle.origin = vec2((symbol_sz.x/2) * pent_scale, (symbol_sz.y/2) * pent_scale)
+pentacle.origin = vec2((float(symbol_sz.x) / 2), 0)
+echo "pentacle x:"
+echo pentacle.origin.x
 
 randomize()
 let symbol_magnitude = rand(1..10)
-let spacing = int(window.size.x / (symbol_magnitude + 2))
 echo symbol_magnitude
 
+# make the 
+let symbol_points: seq[Point] = getSymbolPoints(symbol_magnitude, window.size.x)
+echo symbol_points.len
+
+# The main loop. For web app there will be no loop.
 while window.open:
     var symbol_magnitude_count = 0
     var event: Event
@@ -48,17 +88,11 @@ while window.open:
     
     window.clear color(112, 197, 206)
     window.draw bg
-    window.draw bird
+    #window.draw bird
 
-    while symbol_magnitude_count < symbol_magnitude:
-      if symbol_magnitude != 1:
-        let x_position = (spacing * symbol_magnitude_count) + spacing
-        pentacle.position = vec2(x_position, 100)
-      else:
-        pentacle.position = vec2(window.size.x / 2, 100)
-      
+    for point in symbol_points:
+      pentacle.position = vec2(point.x, point.y)
       window.draw pentacle
-      symbol_magnitude_count += 1
 
     window.display()
 
